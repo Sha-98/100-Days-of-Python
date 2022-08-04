a5720fdf0ec06abbcb9f6aed729d1b6a7a426284
Isha Choudhary <89126969+Sha-98@users.noreply.github.com>
2022-08-04 15:47:25 +0530

    Add files via upload

---
+++ Day_05.py
@@ -0,0 +1,96 @@
+#@##@ Loops #@##@ 
+
+# fruits = ["Apple" , "Orange" , "Peaches"] 
+# for fruit in fruits:
+#     print(fruit + " Pie")
+
+
+
+
+#### Average height
+
+# total = 0
+
+# students_heights = input("Input a list of students heights ").split()
+
+# for n in range(0,len(students_heights)):
+#     students_heights[n] = int(students_heights[n])
+
+# print(students_heights)
+
+
+# for n in students_heights:
+#     total += n
+
+# avg_height = total/len(students_heights)
+# print(avg_height)
+
+
+
+#### Heighest_score
+
+# scores = input("Enter the scores : ").split( )
+
+# heighest_score = 0
+
+# for s in range(0,len(scores)) :
+#     scores[s] = int(scores[s])
+
+# for s in scores:
+#     if s > heighest_score:
+#         heighest_score = s
+
+
+# print(heighest_score)
+
+
+
+
+
+#### Sum of numbers
+
+
+# result = 0
+# for number in range(1,101):
+
+#     result += number
+
+# print(result)
+
+
+
+
+#### Adding even numbers
+
+# numbers = range(0,101,2)
+
+# total = 0
+
+# for n in numbers:
+#     total += n
+
+# print(total)
+
+
+
+
+
+ # FizzBuzz Challange
+
+
+# number = range(0,101)
+
+# for n in number:
+#     if n%3 == 0 and n%5 == 0:
+#         n = "FizzBuzz"
+#     elif n%3 == 0:
+#         n = "Fizz"
+#     elif n%5 == 0:
+#         n = "Fizz"
+#     else :
+#         n = n 
+#     print(n)
+
+
+
+
